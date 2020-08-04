75f98cffc67244dc1e2714c2454384c86ecfce66
quyenhair2109 <quyenphan2109@gmail.com>
2020-08-04 16:38:14 +0700

    xong one page web london, mở project mới

--- london-monaco/london.js
+++ london-monaco/london.js
@@ -2,10 +2,32 @@ $(function () {
     $('.top-menu ul li:nth-child(1) a').on('click',function (e) { 
         e.preventDefault(); /*không để tự chuyển trang vào link href khi click vào*/
         
-        $('body').animate({
-            scrollTop:1500},
-            500, function(){
-
-        });
+        $('body').animate({scrollTop:$('.chapter-1').offset().top},1400, 'easeOutCubic');
     });
+    
+    $('.top-menu ul li:nth-child(2) a').on('click',function (e) { 
+        e.preventDefault(); /*không để tự chuyển trang vào link href khi click vào*/
+        
+        $('body').animate({scrollTop:$('.chapter-2').offset().top},1400, 'easeOutCubic');
+    });
+
+    $('.top-menu ul li:nth-child(3) a').on('click',function (e) { 
+        e.preventDefault(); /*không để tự chuyển trang vào link href khi click vào*/
+        
+        $('body').animate({scrollTop:$('.chapter-3').offset().top},1400, 'easeOutCubic');
+    });
+
+    $('.top-menu ul li:nth-child(4) a').on('click',function (e) { 
+        e.preventDefault(); /*không để tự chuyển trang vào link href khi click vào*/
+        
+        $('body').animate({scrollTop:$('.chapter-4').offset().top},1400, 'easeOutCubic');
+    });
+
+    // viết cho nút comeback
+    $('.comeback').on('click',function (e) { 
+        e.preventDefault(); /*không để tự chuyển trang vào link href khi click vào*/
+        
+        $('body').animate({scrollTop:0},1400, 'easeOutCubic');
+    });
+
 });
