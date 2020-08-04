903d2bfef360849a1b75ec5142b0462d7f3dd802
quyenhair2109 <quyenphan2109@gmail.com>
2020-08-04 15:42:43 +0700

    thêm css cho one page web

--- london-monaco/london.js
+++ london-monaco/london.js
@@ -1,3 +1,11 @@
-$(document).ready(function () {
-   
+$(function () {
+    $('.top-menu ul li:nth-child(1) a').on('click',function (e) { 
+        e.preventDefault(); /*không để tự chuyển trang vào link href khi click vào*/
+        
+        $('body').animate({
+            scrollTop:1500},
+            500, function(){
+
+        });
+    });
 });
