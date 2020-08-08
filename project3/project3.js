e97724222f64d8254f7a6927fb12d95273b06b16
quyenhair2109 <quyenphan2109@gmail.com>
2020-08-08 09:49:56 +0700

    Done project 3 sử dụng animate scroll và gia tốc easing

--- project3/project3.js
+++ project3/project3.js
@@ -1,3 +1,15 @@
-$(document).ready(function () {
-   
+$(function () {
+    $('.block-title').click(function (e) {
+        e.preventDefault();
+        $(this).toggleClass('xanh');
+
+        // click vào tiêu đề khối thì hiện nội dung
+        $(this).next().slideToggle(500, 'easeInOutExpo');
+        
+        // khi ấn vào tiêu đề khối nào thì cuộn đến vị trí khối đó
+        // sử dụng thư viện gia tốc easing cho animate
+        $('body').animate({scrollTop: $(this).offset().top}, 900, 'easeInOutExpo')
+    });
+
+    $('.block-content').slideUp();
 });
