ffc70ab670c1ffe769071b910cc568cd8510345f
quyenhair2109 <quyenphan2109@gmail.com>
2020-08-12 16:17:13 +0700

    hiệu ứng push menu

---
+++ pushMenu/js/pushMenu.js
@@ -0,0 +1,7 @@
+$(function() {
+	$('.nut-mo-ra').click(function (e) { 
+        e.preventDefault();
+        $('.menu').toggleClass('di-vao');
+        $('.contain').toggleClass('di-vao');
+    });
+});
